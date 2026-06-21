6490eb652fd6405fd75049c85574bafccc8c3b57
Gustavo Barragan <dev@droni.co>
2026-06-20 22:36:55 -0500

    fix(site): update droni-kit and cards in blog

--- apps/site/components/Blog/Card.vue
+++ apps/site/components/Blog/Card.vue
@@ -1,27 +1,19 @@
 <template>
-  <article 
+  <DuiCard
     v-if="props.post"
-    class="bg-white border border-gray-200 rounded shadow dark:bg-gray-800 dark:border-gray-700
-    hover:scale-105 transition-transform">
-    <NuxtLink :to="`/blog/${props.post.slug}`">
-      <NuxtImg :src="props.post.picture ?? ''" class="w-full rounded-t aspect-video object-cover" :alt="props.post.name" />
-    </NuxtLink>
-    <div class="p-5">
-      <NuxtLink :to="`/blog/${props.post.slug}`">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-700 dark:text-gray-100 hover:text-gray-900 dark:hover:text-gray-300">
-          {{ props.post.name }}
-        </h5>
-      </NuxtLink>
-      <p class="mb-3 font-normal text-gray-600 dark:text-gray-300">
-        <small class="text-gray-500 dark:text-gray-400 block">
-          By {{ props.post.user?.name }} | {{ props.post.created_at }}
-        </small>
-        {{ props.post.description }}
-      </p>
-    </div>
-  </article>
+    :image="props.post.picture ?? ''"
+    :title="props.post.name"
+    :to="`/blog/${props.post.slug}`"
+    size="l"
+  >
+    <small class="text-gray-500 dark:text-gray-400 block mb-1">
+      By {{ props.post.user?.name }} | {{ props.post.created_at }}
+    </small>
+    {{ props.post.description }}
+  </DuiCard>
 </template>
 <script setup lang="ts">
+import { DuiCard } from '@dronico/droni-kit'
 const props = defineProps<{
   post: Post
 }>()
